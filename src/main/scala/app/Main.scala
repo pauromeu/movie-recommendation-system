5705a76bbe73dbfb8e8e8642dad7d693acd873fb
pauromeu <romeu.pau@gmail.com>
2023-03-21 09:41:17 +0100

    get number of movies rated each year

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,6 +1,7 @@
 package app
 
 import app._
+import app.analytics.SimpleAnalytics
 import app.loaders.{MoviesLoader, RatingsLoader}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -13,7 +14,16 @@ object Main {
     //check ratings loader
     val ratingsLoader = new RatingsLoader(sc, "/ratings_small.csv")
     val ratings  = ratingsLoader.load()
-    ratings.take(5).foreach(rating => println(rating.toString()))
+
+    val moviesLoader = new MoviesLoader(sc, "/movies_small.csv")
+    val movies = moviesLoader.load()
+
+
+    val analytics = new SimpleAnalytics()
+    analytics.init(ratings, movies)
+
+    //analytics.getNumberOfMoviesRatedEachYear.foreach(a => println(a.toString()))
+
 
   }
 }
--- src/main/scala/app/analytics/SimpleAnalytics.scala
+++ src/main/scala/app/analytics/SimpleAnalytics.scala
@@ -6,18 +6,43 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel.MEMORY_AND_DISK
 import org.joda.time.DateTime
 
+import java.time.Instant
+
 
 class SimpleAnalytics() extends Serializable {
 
   private var ratingsPartitioner: HashPartitioner = null
   private var moviesPartitioner: HashPartitioner = null
+  private var ratingsGroupedByYearByTitle: RDD[(Int, Iterable[(Int, Iterable[(Int, Option[Double], Double)])])] = null
+  private var titlesGroupedById: RDD[(Int, (String, List[String]))] = null
+  private val numPartitions: Int = 4
 
   def init(
             ratings: RDD[(Int, Int, Option[Double], Double, Int)],
             movie: RDD[(Int, String, List[String])]
-          ): Unit = ???
+          ): Unit = {
 
-  def getNumberOfMoviesRatedEachYear: RDD[(Int, Int)] = ???
+    ratingsPartitioner = new HashPartitioner(numPartitions)
+    moviesPartitioner = new HashPartitioner(numPartitions)
+
+    titlesGroupedById = movie.map(movie => (movie._1, (movie._2, movie._3)))
+
+    ratingsGroupedByYearByTitle = ratings.map(rating => (unixDate2Year(rating._5), (rating._1, rating._2, rating._3, rating._4)))
+      .groupByKey()
+      .mapValues(_.groupBy(_._2).mapValues(_.map(t => (t._1, t._3, t._4))))
+
+    titlesGroupedById.partitionBy(ratingsPartitioner).persist()
+    ratingsGroupedByYearByTitle.partitionBy(moviesPartitioner).persist()
+
+    //ratingsGroupedByYearByTitle.foreach(a => println(a.toString()))
+  }
+  private def unixDate2Year(unixDate: Int): Int = {
+    val dt = new DateTime(unixDate.toLong * 1000)
+    dt.getYear
+  }
+  def getNumberOfMoviesRatedEachYear: RDD[(Int, Int)] = {
+    ratingsGroupedByYearByTitle.map(yearMovies => (yearMovies._1, yearMovies._2.size))
+  }
 
   def getMostRatedMovieEachYear: RDD[(Int, String)] = ???
 
